2ee00b7a0ea6ffaf7b972d40f065211a82bf3fa1
chungm1 <chungm1@hotmail.com>
2023-01-16 19:33:42 -0500

    update URL

--- client/script.js
+++ client/script.js
@@ -86,8 +86,8 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-//    const response = await fetch('https://codex-im0y.onrender.com/', {
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://openai-sdm7.onrender.com/', {
+//    const response = await fetch('http://localhost:5000/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
